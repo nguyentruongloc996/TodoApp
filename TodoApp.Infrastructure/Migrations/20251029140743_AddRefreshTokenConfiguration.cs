4d7aad15b791ea51aba5aa3141fc76a2f92b147c
Truong Loc <nguyentruongloc996@gmail.com>
2025-10-29 22:12:29 +0700

    Add RefreshToken configuration.

---
+++ TodoApp.Infrastructure/Migrations/20251029140743_AddRefreshTokenConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokenConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Token",
+                table: "RefreshTokens",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_Token",
+                table: "RefreshTokens",
+                column: "Token",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RefreshTokens_Token",
+                table: "RefreshTokens");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Token",
+                table: "RefreshTokens",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500);
+        }
+    }
+}
